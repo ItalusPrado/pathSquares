b76dcd263f353340a5ce5b9ce20e2e76d5102d2c
ItalusPrado <italusprado@acad.ifce.edu.br>
2017-08-24 08:34:11 -0300

    Initial Commit

---
+++ pathSquares/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pathSquares
+//
+//  Created by Italus Rodrigues do Prado on 24/08/17.
+//  Copyright © 2017 Italus Rodrigues do Prado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
